04cfeed9f1bea449dcdcf515fbf61eee1b955eb5
VitorHugo20051 <vitorhugoplayer@gmail.com>
2023-02-03 09:57:21 +0000

    ex1

--- ex 1.py
+++ ex 1.py
@@ -1,3 +1,12 @@
 # dizer se número é positivo, negativo ou neutro
 
 num = int(input('Digite um número: '))
+
+if num < 0:
+    print('Este número é negativo.')
+
+elif num == 0:
+    print('Este número é neutro.')
+
+else:
+    print('Este número é positivo.')
