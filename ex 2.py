990917a050c039a3d84203ea77b383c342639358
VitorHugo20051 <vitorhugoplayer@gmail.com>
2023-02-03 13:36:33 +0000

    ex 2

---
+++ ex 2.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+if num == num2:
+    print('Os números digitados são iguais.')
+
+else:
+    print('Os números digitados são diferentes.')
