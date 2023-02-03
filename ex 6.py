22c2db7d1caf27517f86e52684fa538228606d3f
VitorHugo20051 <vitorhugoplayer@gmail.com>
2023-02-03 13:57:20 +0000

    ex6

---
+++ ex 6.py
@@ -0,0 +1,9 @@
+# informar se um numero é divisivel por 5
+
+num = int(input('Digite um número: '))
+
+if num % 5 == 0:
+    print('Este número é  divisel por 5.')
+
+else:
+    print('Este número não é divisivel por 5.')
